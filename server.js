91f273bc6ee558914b083d5b6b7a2f6f1bb0b91f
Luke Taylor <lmcjt@outlook.com>
2016-12-17 21:55:26 +0000

    :sparkles: added ability to add new user to db

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 const mongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
+const bodyParser = require('body-parser');
 
 const port = 3001;
 const mongoDBPort = 37001;
@@ -11,9 +12,17 @@ const ip = internalIp.v4();
 // Connection URL
 const url = 'mongodb://localhost:' + mongoDBPort + '/random-user-api';
 
+// Server Configuration
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+    res.redirect("/health-check");
+});
+
 app.get('/health-check', (req, res) => {
     // Use connect method to connect to the server
-    mongoClient.connect(url, function(err, db) {
+    mongoClient.connect(url, (err, db) => {
       assert.equal(null, err);
 
       res.writeHead(200, {"Content-Type": "text/plain"});
@@ -23,9 +32,32 @@ app.get('/health-check', (req, res) => {
     });
 });
 
-app.get('/', (req, res) => {
-    res.redirect("/health-check");
-});
+app.route('/add-user')
+    .get((req, res, next) => {
+        res.sendFile(__dirname + "/views/add-user.html");
+    })
+    .post((req, res, next) => {
+        mongoClient.connect(url, (err, db) => {
+            assert.equal(null, err);
+            const collection = db.collection('users');
+            collection.insertOne({
+                "firstName": req.body.firstName,
+                "lastName": req.body.lastName,
+            }, {
+                w: 'majority',
+                wtimeout: 10000,
+                serializeFunctions: true
+            }, (err, result) => {
+                assert.equal(err, null);
+                assert.equal(1, result.insertedCount);
+
+                res.writeHead(200, {"Content-Type": "text/plain"});
+                res.end("Successfully added " + req.body.firstName + " " + req.body.lastName + " to database.\n");
+
+                db.close();
+            });
+        });
+    });
 
 app.listen(port, (err) => {
     if (err) {

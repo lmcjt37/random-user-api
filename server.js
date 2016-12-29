736a085615c76b10539e2636b123c4ba62cca6fc
lmcjt37 <lmcjt@outlook.com>
2016-12-29 17:36:52 +0000

    :sparkles: added extra search field for get user and added delete method

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const internalIp = require('internal-ip');
 const express = require('express');
 const app = express();
 const mongoClient = require('mongodb').MongoClient;
+const ObjectId = require('mongodb').ObjectId;
 const assert = require('assert');
 const bodyParser = require('body-parser');
 
@@ -64,7 +65,7 @@ app.get('/get-users', (req, res) => {
     mongoClient.connect(url, (err, db) => {
         assert.equal(null, err);
 
-        var collection = db.collection('users');
+        const collection = db.collection('users');
         collection.find({}).toArray((err, docs) => {
             assert.equal(err, null);
             assert.notEqual(docs.length, 0);
@@ -92,8 +93,11 @@ app.route('/get-user')
             if (req.body.lastName) {
                 query.lastName = req.body.lastName;
             }
+            if (req.body.uid) {
+                query._id = ObjectId(req.body.uid);
+            }
 
-            var collection = db.collection('users');
+            const collection = db.collection('users');
             collection.find(query).toArray((err, docs) => {
                 assert.equal(err, null);
                 assert.notEqual(docs.length, 0);
@@ -104,6 +108,34 @@ app.route('/get-user')
 
                 db.close();
             });
+        });
+    });
+
+app.route('/delete-user')
+    .get((req, res, next) => {
+        res.sendFile(__dirname + "/views/delete-user.html");
+    })
+    .post((req, res, next) => {
+        mongoClient.connect(url, (err, db) => {
+            assert.equal(null, err);
+
+            const query = {};
+            if (req.body.uid) {
+                query._id = ObjectId(req.body.uid);
+            }
+
+            const collection = db.collection('users');
+            assert.equal(query.hasOwnProperty("_id"), true);
+            collection.findOneAndDelete(query, (err, result) => {
+                assert.equal(err, null);
+
+                res.set('Content-Type', 'text/plain');
+                res.status(200);
+                res.send("Removed the document with the uid: " + query._id);
+
+                db.close();
+            });
+
         });
     });
 
